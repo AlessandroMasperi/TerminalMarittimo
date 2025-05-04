c4aa6de89f5d6874a66ac22791d1b965e72d5251
DESKTOP-MKKAHMG\Ale <alemasverano@gmail.com>
2025-05-04 17:38:14 +0200

    Creazione del progetto con SpringBoot

---
+++ terminal_marittimo/src/main/java/com/example/terminal_marittimo/TerminalMarittimoApplication.java
@@ -0,0 +1,13 @@
+package com.example.terminal_marittimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminalMarittimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerminalMarittimoApplication.class, args);
+	}
+
+}
---
+++ terminal_marittimo/src/test/java/com/example/terminal_marittimo/TerminalMarittimoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.terminal_marittimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerminalMarittimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
